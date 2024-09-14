3c8b43df02125e9d00b23d64d9dabbb3277f9e8c
Prakriti Chauhan <95423965+Prakriti1207@users.noreply.github.com>
2024-09-14 18:24:39 +0530

    Create App.js

---
+++ lient/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import LectureList from './LectureList';
+
+function App() {
+  return (
+    <div>
+      <h1>Virtual Classroom</h1>
+      <LectureList />
+    </div>
+  );
+}
+
+export default App;
