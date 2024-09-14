2d91cd2a71c80b815e394e680bf8a4b6f24a7521
Prakriti Chauhan <95423965+Prakriti1207@users.noreply.github.com>
2024-09-14 18:43:07 +0530

    Create updatesserver.js

---
+++ server/updatesserver.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const Lecture = require('./models/Lecture');
+const Comment = require('./models/Comment');
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/lectures', async (req, res) => {
+  const lectures = await Lecture.find().populate('comments');
+  res.json(lectures);
+});
+
+app.post('/api/comments/:lectureId', async (req, res) => {
+  const lectureId = req.params.lectureId;
+  const commentText = req.body.commentText;
+  const comment = new Comment({ lectureId, commentText });
+  await comment.save();
+  res.json(comment);
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
